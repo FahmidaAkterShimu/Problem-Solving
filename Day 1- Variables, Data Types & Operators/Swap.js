5e8df984bb606b5c5d36b75c522615a6e0a9e9ce
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-09-15 20:12:15 +0600

    Problem 1: Swap Two Variables

--- Day 1- Variables, Data Types & Operators/Swap.js
+++ Day 1- Variables, Data Types & Operators/Swap.js
@@ -1,4 +1,8 @@
-// Problem 1: Swap Two Variables
+/* Problem 1: Swap Two Variables
+ Description: Write a function that swaps the values of two variables without using a third variable.
+ Example:
+ Input: a = 5, b = 10Output: a = 10, b = 5
+ Hint: Try using destructuring or arithmetic operators. */
 
 function swap(a, b) {
     [a, b] = [b, a];
