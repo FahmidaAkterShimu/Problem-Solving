3dc80968d9da7fb3e73e3b6c509581c296c3e2b2
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-09-17 21:47:44 +0600

    Problem 9: Capitalize First Letter of Each Word

---
+++ Day 2 - Strings & String Methods/9_titleCase.js
@@ -0,0 +1,17 @@
+/* Problem 9: Capitalize First Letter of Each Word
+ Description: Write a function titleCase(str) that capitalizes the first letter of every word in a string.
+ Example:
+ Input: 'hello world'  → Output: 'Hello World'
+ Hint: Use split(' '), map(), and join(' ').
+ */
+
+function titleCase(str) {
+    return str
+        .split(" ")
+        .map(word => word[0].toUpperCase() + word.slice(1))
+        .join(" ");
+}
+
+output = titleCase("hello world");
+
+console.log(output);
